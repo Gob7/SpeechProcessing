e38316aed39ddaedf0eab7f8b26d8c02c2b8ff8e
Gob7 <pandagobinda7@gmail.com>
2025-10-27 20:38:05 +0530

    organise

--- yes_no_2/yes_no_2/yes_no_2.cpp
+++ yes_no_2/yes_no_2/yes_no_2.cpp
@@ -9,15 +9,99 @@ using namespace std;
 #define noise 20000
 #define yes 40
 short int waveIn[16025 * 3];
-
 #pragma comment(lib, "winmm.lib")
-void PlayRecord();
-void writedataTofile(LPSTR lpData, DWORD dwBufferLength);
-void StartRecord();
+
+void PlayRecord()
+{
+	// 'short int' is a 16-bit type; I request 16-bit samples below
+	// for 8-bit capture, you'd    use 'unsigned char' or 'BYTE' 8-bit types
+	const int NUMPTS = 16025 * 3; // 3 seconds
+	int sampleRate = 16025;
+
+	// Specify recording parameters
+	HWAVEIN hWaveIn;
+	WAVEFORMATEX pFormat;
+	pFormat.wFormatTag = WAVE_FORMAT_PCM;	  // simple, uncompressed format
+	pFormat.nChannels = 1;					  //  1=mono, 2=stereo
+	pFormat.nSamplesPerSec = sampleRate;	  // 44100
+	pFormat.nAvgBytesPerSec = sampleRate * 2; // = nSamplesPerSec * n.Channels * wBitsPerSample/8
+	pFormat.nBlockAlign = 2;				  // = n.Channels * wBitsPerSample/8
+	pFormat.wBitsPerSample = 16;			  //  16 for high quality, 8 for telephone-grade
+	pFormat.cbSize = 0;
+
+	// Set up and prepare header for input
+	waveInOpen(&hWaveIn, WAVE_MAPPER, &pFormat, 0L, 0L, WAVE_FORMAT_DIRECT);
+
+	WAVEHDR WaveInHdr;
+	WaveInHdr.lpData = (LPSTR)waveIn;
+	WaveInHdr.dwBufferLength = NUMPTS * 2;
+	WaveInHdr.dwBytesRecorded = 0;
+	WaveInHdr.dwUser = 0L;
+	WaveInHdr.dwFlags = 0L;
+	WaveInHdr.dwLoops = 0L;
+
+	waveInPrepareHeader(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
+
+	HWAVEOUT hWaveOut;
+
+	cout << "playing..." << endl;
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &pFormat, 0, 0, WAVE_FORMAT_DIRECT);
+	waveOutWrite(hWaveOut, &WaveInHdr, sizeof(WaveInHdr)); // Playing the data
+	Sleep(3 * 1000);									   // Sleep for as long as there was recorded
+
+	waveInClose(hWaveIn);
+	waveOutClose(hWaveOut);
+}
+
+void StartRecord()
+{
+	// 'short int' is a 16-bit type; I request 16-bit samples below
+	// for 8-bit capture, you'd use 'unsigned char' or 'BYTE' 8-bit types
+	const int NUMPTS = 16025 * 3; // 3 seconds
+	int sampleRate = 16025;
+
+	// Specify recording parameters
+	HWAVEIN hWaveIn;
+	MMRESULT result;
+	WAVEFORMATEX pFormat;
+	pFormat.wFormatTag = WAVE_FORMAT_PCM;	  // simple, uncompressed format
+	pFormat.nChannels = 1;					  //  1=mono, 2=stereo
+	pFormat.nSamplesPerSec = sampleRate;	  // 8.0 kHz, 11.025 kHz, 22.05 kHz, and 44.1 kHz
+	pFormat.nAvgBytesPerSec = sampleRate * 2; // =  nSamplesPerSec * nBlockAlign
+	pFormat.nBlockAlign = 2;				  // = (nChannels * wBitsPerSample) / 8
+	pFormat.wBitsPerSample = 16;			  //  16 for high quality, 8 for telephone-grade
+	pFormat.cbSize = 0;
+
+	// Set up and prepare header for input
+	result = waveInOpen(&hWaveIn, WAVE_MAPPER, &pFormat, 0L, 0L, WAVE_FORMAT_DIRECT);
+
+	WAVEHDR WaveInHdr;
+	WaveInHdr.lpData = (LPSTR)waveIn;
+	WaveInHdr.dwBufferLength = NUMPTS * 2;
+	WaveInHdr.dwBytesRecorded = 0;
+	WaveInHdr.dwUser = 0L;
+	WaveInHdr.dwFlags = 0L;
+	WaveInHdr.dwLoops = 0L;
+
+	// Insert a wave input buffer
+	waveInPrepareHeader(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
+
+	// Commence sampling input
+	result = waveInAddBuffer(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
+
+	// Wait until finished recording
+	result = waveInStart(hWaveIn);
+	cout << "recording for 3 seconds..." << endl;
+	Sleep(3 * 1000);
+
+	waveInClose(hWaveIn);
+	// PlayRecord();
+}
 
 int _tmain(int argc, _TCHAR *argv[])
 {
 	StartRecord();
+
 	// used volt instead of energy
 	int volt[sample / range], zcr[sample / range];
 	int start, end, i, j, k, count1, count2;
@@ -77,77 +161,4 @@ int _tmain(int argc, _TCHAR *argv[])
 	else if (count1 < count2)
 		printf("\nYES\n");
 	return 0;
-}
-
-void StartRecord()
-{
-	const int NUMPTS = 16025 * 3; // 3 seconds
-	int sampleRate = 16025;
-	// 'short int' is a 16-bit type; I request 16-bit samples below
-	// for 8-bit capture, you'd use 'unsigned char' or 'BYTE' 8-bit     types
-	HWAVEIN hWaveIn;
-	MMRESULT result;
-	WAVEFORMATEX pFormat;
-	pFormat.wFormatTag = WAVE_FORMAT_PCM;	  // simple, uncompressed format
-	pFormat.nChannels = 1;					  //  1=mono, 2=stereo
-	pFormat.nSamplesPerSec = sampleRate;	  // 8.0 kHz, 11.025 kHz, 22.05 kHz, and 44.1 kHz
-	pFormat.nAvgBytesPerSec = sampleRate * 2; // =  nSamplesPerSec � nBlockAlign
-	pFormat.nBlockAlign = 2;				  // = (nChannels � wBitsPerSample) / 8
-	pFormat.wBitsPerSample = 16;			  //  16 for high quality, 8 for telephone-grade
-	pFormat.cbSize = 0;
-	// Specify recording parameters
-	result = waveInOpen(&hWaveIn, WAVE_MAPPER, &pFormat, 0L, 0L, WAVE_FORMAT_DIRECT);
-	WAVEHDR WaveInHdr;
-	// Set up and prepare header for input
-	WaveInHdr.lpData = (LPSTR)waveIn;
-	WaveInHdr.dwBufferLength = NUMPTS * 2;
-	WaveInHdr.dwBytesRecorded = 0;
-	WaveInHdr.dwUser = 0L;
-	WaveInHdr.dwFlags = 0L;
-	WaveInHdr.dwLoops = 0L;
-	waveInPrepareHeader(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
-	// Insert a wave input buffer
-	result = waveInAddBuffer(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
-	// Commence sampling input
-	result = waveInStart(hWaveIn);
-	cout << "recording for 3 seconds..." << endl;
-	Sleep(3 * 1000);
-	// Wait until finished recording
-	waveInClose(hWaveIn);
-	// PlayRecord();
-}
-
-void PlayRecord()
-{
-	const int NUMPTS = 16025 * 3; // 3 seconds
-	int sampleRate = 16025;
-	// 'short int' is a 16-bit type; I request 16-bit samples below
-	// for 8-bit capture, you'd    use 'unsigned char' or 'BYTE' 8-bit types
-	HWAVEIN hWaveIn;
-	WAVEFORMATEX pFormat;
-	pFormat.wFormatTag = WAVE_FORMAT_PCM;	  // simple, uncompressed format
-	pFormat.nChannels = 1;					  //  1=mono, 2=stereo
-	pFormat.nSamplesPerSec = sampleRate;	  // 44100
-	pFormat.nAvgBytesPerSec = sampleRate * 2; // = nSamplesPerSec * n.Channels * wBitsPerSample/8
-	pFormat.nBlockAlign = 2;				  // = n.Channels * wBitsPerSample/8
-	pFormat.wBitsPerSample = 16;			  //  16 for high quality, 8 for telephone-grade
-	pFormat.cbSize = 0;
-	// Specify recording parameters
-	waveInOpen(&hWaveIn, WAVE_MAPPER, &pFormat, 0L, 0L, WAVE_FORMAT_DIRECT);
-	WAVEHDR WaveInHdr;
-	// Set up and prepare header for input
-	WaveInHdr.lpData = (LPSTR)waveIn;
-	WaveInHdr.dwBufferLength = NUMPTS * 2;
-	WaveInHdr.dwBytesRecorded = 0;
-	WaveInHdr.dwUser = 0L;
-	WaveInHdr.dwFlags = 0L;
-	WaveInHdr.dwLoops = 0L;
-	waveInPrepareHeader(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
-	HWAVEOUT hWaveOut;
-	cout << "playing..." << endl;
-	waveOutOpen(&hWaveOut, WAVE_MAPPER, &pFormat, 0, 0, WAVE_FORMAT_DIRECT);
-	waveOutWrite(hWaveOut, &WaveInHdr, sizeof(WaveInHdr)); // Playing the data
-	Sleep(3 * 1000);									   // Sleep for as long as there was recorded
-	waveInClose(hWaveIn);
-	waveOutClose(hWaveOut);
 }
